1345256de3e2417aa2228822af0d3ccd411e9cda
Rushi <148181969+RG4025@users.noreply.github.com>
2024-01-06 10:33:21 +0000

    New Commit

---
+++ FirstFile/Main.java
@@ -0,0 +1,10 @@
+// import java.lang.*;
+// import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Printing the name amd welcome message : ");
+        String name = "Rushikesh";
+        System.out.println("Hello " + name + " Welcome to the java!..");
+    }
+}
