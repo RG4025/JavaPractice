08e3eaa1c24a667e1de2cca5cbf63a054c0e17c1
Rushi <148181969+RG4025@users.noreply.github.com>
2024-01-08 12:51:57 +0000

    New Commit

---
+++ FirstFile/Second.java
@@ -0,0 +1,26 @@
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+public class Second {
+
+    public static void main(String[] args) {
+        int high =  Integer.MAX_VALUE;
+        int low = Integer.MIN_VALUE;
+
+        System.out.println(high+ " \n " + low);
+
+        Long val1 = 127L;
+        Long val2 = 127L;
+
+        System.out.println(val1 == val2);
+
+        Long val3 = 128L;
+        Long val4 = 128L;
+
+        System.out.println(val3 == val4);
+
+        ZoneId zone1 = ZoneId.of("Asia/Kolkata");
+        LocalTime notTime = LocalTime.now(zone1);
+        System.out.println(notTime);
+    }
+}
