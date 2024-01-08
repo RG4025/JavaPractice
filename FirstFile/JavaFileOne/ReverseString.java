47c3842539ffe553cf227918824e1cb88d76b7c4
Rushi <148181969+RG4025@users.noreply.github.com>
2024-01-08 10:04:28 +0000

    New Commit

---
+++ FirstFile/JavaFileOne/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        
+        System.out.println("Printing the Reverse String Usig the Strig reverse And slit method:");
+
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter the String below :");
+        String str = obj.next();
+
+
+    }
+
+   
+}
