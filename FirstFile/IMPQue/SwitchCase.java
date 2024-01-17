db69fee8adbb91972f87e44e28e7dc804b285b60
Rushi <148181969+RG4025@users.noreply.github.com>
2024-01-17 04:29:52 +0000

    New Commit

---
+++ FirstFile/IMPQue/SwitchCase.java
@@ -0,0 +1,11 @@
+package FirstFile.IMPQue;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        
+        System.out.println("Enhanced Switch Case!!.");
+
+
+        
+    }
+}
