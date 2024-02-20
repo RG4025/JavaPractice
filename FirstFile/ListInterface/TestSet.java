a3ed1c892728eb641d2d6376a4782b097e407ad7
Rushi <148181969+RG4025@users.noreply.github.com>
2024-02-20 09:58:04 +0000

    New Commit

--- FirstFile/ListInterface/TestSet.java
+++ FirstFile/ListInterface/TestSet.java
@@ -1,6 +1,7 @@
 package FirstFile.ListInterface;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class TestSet {
@@ -21,6 +22,19 @@ public class TestSet {
         System.out.println(names.remove("Dilip"));
         // can't reemove the unpresented data from the set then it will return the false 
         System.out.println(names.remove("Dilip"));
+
+
+        // the linkedHashSet Only stores the unique values it doesnt store the repeaed values.
+
+        Set<String> linkName = new LinkedHashSet<>();
+        linkName.add("Rushi");
+        linkName.add("Gurav");
+        linkName.add("Gauri");
+        linkName.add("Gurav");
+
+        System.out.println(linkName);
+
+        
         
     }
 }
